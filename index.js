1dc377745684c9b47ed696296facf1845cf77c2d
Giovani <desouzagiovani9@gmail.com>
2023-05-16 20:38:00 +0100

    Adding images on uploads folder

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-const {
-  S3Client,
-  PutObjectCommand,
-  GetObjectCommand,
-} = require("@aws-sdk/client-s3");
-const sharp = require("sharp");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 require("dotenv").config();
 
 const app = express();
@@ -15,16 +8,18 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+app.use("/uploads", express.static("uploads"));
 
-const s3 = new S3Client({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
   },
-  region: process.env.BUCKET_REGION,
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
 });
+
+const upload = multer({ storage });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -39,34 +34,8 @@ app.post("/upload", (req, res) => {
     console.log("req.body", req.body);
     console.log("req.file", req.file);
 
-    // resize image image if you want
-
-    /*const buffer = await sharp(req.file.buffer)
-      .resize({ height: 1600, width: 840, fit: "contain" })
-      .toBuffer();
-      */
-    const imageName = Date.now() + req.file.originalname;
-    const params = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: imageName,
-      Body: req.file.buffer,
-      ContentType: req.file.mimetype,
-    };
-
-    const commandToCreate = new PutObjectCommand(params);
-    await s3.send(commandToCreate);
-
-    const getObjectParams = {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: imageName,
-    };
-
-    const commandToGet = new GetObjectCommand(getObjectParams);
-
-    const imageLink = await getSignedUrl(s3, commandToGet);
-
     res.json({
-      data: { imageLink, ...req.body },
+      data: { fileName: req.file.filename, ...req.body },
     });
   });
 });
